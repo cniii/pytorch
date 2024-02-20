1d82c79fed8d60c1bd6af1d9fce45bcd4fe11cea
DanilBaibak <baibak@meta.com>
2024-02-20 13:38:28 +0100

    Fix bash command

--- .ci/docker/build.sh
+++ .ci/docker/build.sh
@@ -71,7 +71,7 @@ if [[ "$image" == *cuda* && "$UBUNTU_VERSION" != "22.04" ]]; then
   DOCKERFILE="${OS}-cuda/Dockerfile"
 elif [ "$3" = "ARC" ]; then
   DOCKERFILE="${OS}-arc/Dockerfile"
-  shift
+  set -- "${@:1:2}" "${@:4}"
 elif [[ "$image" == *rocm* ]]; then
   DOCKERFILE="${OS}-rocm/Dockerfile"
 elif [[ "$image" == *xpu* ]]; then
