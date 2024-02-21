345e49079ef26bee6200b81e77d6db5815f69a40
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2024-02-20 17:34:38 -0800

    [dtensor][2/N] add torchrec table-wise sharding example
    ghstack-source-id: 8906dba96a81209e08407b450406ce008fe11645
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120265

--- test/distributed/_tensor/test_dtensor_sharding.py
+++ test/distributed/_tensor/test_dtensor_sharding.py
@@ -4,7 +4,7 @@
 import torch
 
 from torch.distributed._tensor import DeviceMesh, distribute_tensor, DTensor
-from torch.distributed._tensor.placement_types import Shard
+from torch.distributed._tensor.placement_types import Replicate, Shard
 from torch.testing._internal.common_utils import run_tests
 from torch.testing._internal.distributed._tensor.common_dtensor import (
     DTensorTestBase,
@@ -59,6 +59,33 @@ class DTensorShardingTest(DTensorTestBase):
             local_shard, device_mesh, row_wise_sharding_placements, run_check=False
         )
         self.assertEqual(dtensor.full_tensor(), global_tensor)
+
+    @with_comms
+    def test_dtensor_table_wise_sharding(self):
+        # initialize N tables
+        table_shape = [4, 4]
+        table_to_local_shard = {}  # map {table_id: local shard of table_id}
+        for i in range(self.world_size):
+            local_shard = torch.randn(*table_shape)
+            # table i is placed on rank i
+            table_to_local_shard[i] = local_shard if self.rank == i else torch.empty(0)
+
+        # example 1: transform local_shards into DTensor
+        table_to_dtensor = {}  # same purpose as _model_parallel_name_to_sharded_tensor
+        for table_id, local_shard in table_to_local_shard.items():
+            placements = [Replicate()]  # table-wise sharding
+            device_mesh = DeviceMesh(
+                self.device_type, [table_id]  # table i is placed on rank i
+            )
+            dtensor = DTensor.from_local(
+                local_shard, device_mesh, placements, run_check=False
+            )
+            table_to_dtensor[table_id] = dtensor
+
+        # example 2: transform DTensor into torch.Tensor
+        for table_id, local_shard in table_to_local_shard.items():
+            dtensor_local_shard = table_to_dtensor[table_id].to_local()
+            self.assertEqual(dtensor_local_shard, local_shard)
 
 
 if __name__ == "__main__":
